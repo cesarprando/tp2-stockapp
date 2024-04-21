951bbced525bbeb13bb3c9794b1cef3a7efdcf18
cesarprando <cesarprandosz@gmail.com>
2024-04-21 20:59:27 -0300

    Avaliação

--- StockApp/StockApp.Domain/Entities/Product.cs
+++ StockApp/StockApp.Domain/Entities/Product.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace StockApp.Domain.Entities
 {
@@ -14,18 +15,27 @@ namespace StockApp.Domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Stock { get; set;}
+        public int Stock { get; set; }
         public string Image { get; set; }
         public int CategoryId { get; set; }
         #endregion
 
-        public Product()
+        #region Construtores
+        public Product(string name, string description, decimal price, int stock, string image)
         {
-
+            ValidateDomain(name, description, price, stock, image);
         }
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+        #endregion
 
         public Category Category { get; set; }
 
+        #region
         private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name),
@@ -35,17 +45,20 @@ namespace StockApp.Domain.Entities
                 "Invalid name, too short, minimum 3 characters.");
 
             DomainExceptionValidation.When(string.IsNullOrEmpty(description),
-                "Invalid description, name is required.");
+                "Invalid description, description is required.");
 
             DomainExceptionValidation.When(description.Length < 5,
                 "Invalid description, too short, minimum 5 characters.");
 
-            DomainExceptionValidation.When(price < 0, "Invalid price negative value.");
+            DomainExceptionValidation.When(price < 0,
+                "Invalid price negative value.");
 
-            DomainExceptionValidation.When(stock < 0, "Invalid stock negative value.");
+            DomainExceptionValidation.When(stock < 0,
+                "Invalid stock negative value.");
 
-            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximum 250 characters.");
-
+            DomainExceptionValidation.When(image.Length > 250,
+                "Invalid image name, too long, maximum 250 characters.");
         }
+        #endregion
     }
 }
